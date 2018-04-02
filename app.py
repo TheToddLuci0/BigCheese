54148bec771080a7dd52bf0b01fb8c18a795136c
wrickert <brickert7@gmail.com>
2018-04-02 18:09:08 -0500

    Hello, Miguel

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
 
 
 if __name__ == '__main__':
