fd58abde49e4d6c99f6e6c807e64f71796394bb1
Logan <woolery@iastate.edu>
2018-04-02 21:53:22 -0500

    Add static elements

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/employer/<name>')
+def employer(name):
+    return render_template('employer.html', name=name)
+
+
+@app.route('/employers/')
+def employers():
+    return render_template('employers.html')
+
+
+@app.route('/user/')
+@app.route('/user/<username>')
+def profile(username):
+    return render_template('userProfile.html', username=username)
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
