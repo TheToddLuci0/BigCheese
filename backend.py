c7ffe169c0e373432ca5b2d83d4577619e117ef5
wrickert <brickert7@gmail.com>
2018-04-04 12:56:52 -0500

    changed psql statment

--- backend.py
+++ backend.py
@@ -14,7 +14,7 @@ def getCompany():
     return string
 
 def addCompany(companyName, about):
-    conn = psycopg2.connect("dbname=theCellar user=postgres")
+    conn = psycopg2.connect("dbname=theCellar user=postgres pass=steve")
     cur = conn.cursor()
     print(companyName, about)
     sql = "INSERT INTO COMPANY (NAME,ABOUT,RATE,NUM_RATE) VALUES (%s,%s,%s,%s);"
