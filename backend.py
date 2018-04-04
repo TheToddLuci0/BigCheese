13e368bc9a05d247aa6966969abe2fc1735083ee
wrickert <brickert7@gmail.com>
2018-04-04 14:22:59 -0500

    Do I need this each time?

--- backend.py
+++ backend.py
@@ -4,13 +4,14 @@ def connect():
     conn = psycopg2.connect("dbname=theCellar user=postgres password=steve host=localhost")
 
 def getCompany():
-    string = list()
+    string[10][2] = list()
     conn = psycopg2.connect("dbname=theCellar user=postgres password=steve host=localhost")
     cur = conn.cursor()
     cur.execute("SELECT * FROM company")
     output = cur.fetchall()
     for i in output:
-        string.append(i[0])
+        string[i][0].append(i[0])
+        string[i][1].append(i[1])
     return string
 
 def addCompany(companyName, about):
