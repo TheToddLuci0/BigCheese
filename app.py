3c93781064d07bbb4e1910655fc91d2d569230ce
wrickert <brickert7@gmail.com>
2018-04-09 17:34:08 -0500

    Troubleshooting

--- app.py
+++ app.py
@@ -92,6 +92,7 @@ def profile(display_name):
 @app.route('/check/', methods=['POST', 'GET'])
 def checkPassword():
     if request.method == 'POST':
+        print("Are you here?")
         result = request.form
         print(result)
         success = backend.checkPassword(result['email'],result['password'])
