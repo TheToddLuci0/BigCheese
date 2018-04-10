3917dacf0bfb61197140b55152799e1441fd5852
wrickert <brickert7@gmail.com>
2018-04-10 13:27:24 -0500

    Fuck print statments

--- app.py
+++ app.py
@@ -170,6 +170,4 @@ def displayCompany():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, filename="/var/log/gunicorn/logfile", filemode="a+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
     app.run()
