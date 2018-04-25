60be7aa58be69c61e85290665907cadc0888b080
wrickert <brickert7@gmail.com>
2018-04-24 19:09:53 -0500

    Put it back

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def employer(name):
     if request.cookies.get('loggedIn'):
         if request.method == 'POST':
             result = request.form
-            #backend.addReview(result['Name'], result['Review'], int(result['Score']), request.cookies.get('username'))
+            backend.addReview(result['Name'], result['Review'], int(result['Score']), request.cookies.get('username'))
             return render_template('employers.html', result=backend.getCompany())
             # return redirect("http://www.bigcheese.review/employers")#.format(result['Name']))
         #return redirect("http://www.bigcheese.review/employers")
